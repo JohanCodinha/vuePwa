909e4a9b9e86637624e671556705d2f1a7ed967c
Johan Codinha <johan@codinha.net>
2017-09-20 21:46:27 +1000

    Add test file for account store module

--- src/store/index.test.js
+++ src/store/index.test.js
@@ -2,7 +2,13 @@
 import store from '../store';
 
 describe('Modules should be namespaced', () => {
-  test('account', () => {
-    expect(true).toBe(false)
-  })
+  test('Account is accesible', () => {
+    //console.log(store)
+    expect(store.getters['account/status']).toBe(null);
+  });
+  
+  test('Explore is accesible',() => {
+    
+  });
 })
+
---
+++ src/store/modules/account.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import store from '../index';
+
+describe('Login user', () => {
+ test('User can login', () => {
+   // console.log(store);
+   expect(true).toBe(true);
+ }); 
+});
