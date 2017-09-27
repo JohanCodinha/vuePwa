b479e7cab12c88bd60590fc0a57d6437dc0126b3
Johan Codinha <johan@codinha.net>
2017-09-27 16:17:05 +1000

    build

--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,21 @@ let path = require('path');
 let serveStatic = require('serve-static');
 app = express();
 
+/* Redirect http to https */
+app.get('*', function(req,res,next) {
+  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
+    res.redirect('https://'+req.hostname+req.url)
+  else
+    next() /* Continue to other routes if we're not redirecting */
+});
+
 app.use(serveStatic(__dirname));
 
-let port = process.env.PORT || 5000;
+app.get('/*', function(req,res,next) {
+  console.log('redirect');
+  res.redirect('/')
+});
+
+let port = process.env.PORT || 5050;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started '+ port);/* eslint-disable */
