12810a1fa6dd8fb781a079bf443333edab0d5cfe
Johan Codinha <johan@codinha.net>
2017-09-27 14:03:19 +1000

    build

---
+++ dist/server.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+let express = require('express');
+let path = require('path');
+let serveStatic = require('serve-static');
+app = express();
+
+app.use(serveStatic(__dirname));
+
+let port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
