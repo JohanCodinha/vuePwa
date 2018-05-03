7494a5c144855c4219f5b2d38762026dbb429dd6
Johan Codinha <johan@codinha.net>
2018-05-03 14:24:15 +1000

    build

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,6 +2,7 @@ self.addEventListener('fetch', (event) => {
   console.log('custom fetch', event);
   if (event.request.mode === 'navigate') {
     event.respondWith(
+
       fetch(event.request)
         .catch(() => caches.match('index.html', { ignoreSearch: true })),
     );
